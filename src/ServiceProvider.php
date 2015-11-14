0471df10751abcd8ab92c40981aaa3eceb53c0b1
Christopher Moore <chris@nuwavecommerce.com>
2015-11-14 09:30:27 -0700

    created service provider

---
+++ src/ServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Nuwave\Relay;
+
+use Illuminate\Support\ServiceProvider as BaseProvider;
+
+class ServiceProvider extends BaseProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->commands([
+            Nuwave\Relay\Commands\SchemaCommand::class,
+        ]);
+    }
+}
