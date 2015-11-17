a4c05d30c25d5ba543872e5f60b1b9fb340787a9
Christopher Moore <chris@nuwavecommerce.com>
2015-11-17 05:24:41 -0700

    test encoding of id

--- tests/ObjectIdentificationTest.php
+++ tests/ObjectIdentificationTest.php
@@ -2,7 +2,7 @@
 
 namespace Nuwave\Relay\Tests;
 
-use Nuwave\Relay\Tests\Assets\HumanType;
+use Nuwave\Relay\Tests\Assets\Types\HumanType;
 
 class ObjectIdentificationTest extends BaseTest
 {
@@ -19,5 +19,21 @@ class ObjectIdentificationTest extends BaseTest
             'id' => $encodedId,
             'name' => 'Luke Skywalker'
         ], array_get($response, 'data.node'));
+    }
+
+    /**
+     * @test
+     */
+    public function itEncodesIdForResponse()
+    {
+        $name = 'Darth Vader';
+        $encodedId = base64_encode(HumanType::class . ':' . '1001');
+        $query = '{humanByName(name: "'. $name .'"){id, name}}';
+        $response = $this->graphqlResponse($query);
+
+        $this->assertEquals([
+            'id' => $encodedId,
+            'name' => $name
+        ], array_get($response, 'data.humanByName'));
     }
 }
