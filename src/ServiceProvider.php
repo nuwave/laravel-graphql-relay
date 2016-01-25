815866a93dfc0f7992523c5d2bc2b67b4aeabda3
Christopher Moore <chris@nuwavecommerce.com>
2016-01-25 11:05:34 -0700

    registered generateor and facade

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Nuwave\Relay;
 
+use Nuwave\Relay\Schema\SchemaContainer;
 use Illuminate\Support\ServiceProvider as BaseProvider;
 
 class ServiceProvider extends BaseProvider
@@ -13,7 +14,9 @@ class ServiceProvider extends BaseProvider
      */
     public function boot()
     {
-        //
+        $this->publishes([__DIR__ . '../config/config.php' => config_path('relay.php')]);
+
+        $this->registerSchema();
     }
 
     /**
@@ -26,5 +29,26 @@ class ServiceProvider extends BaseProvider
         $this->commands([
             \Nuwave\Relay\Commands\SchemaCommand::class,
         ]);
+
+        $this->app->singleton('relay', function ($app) {
+            return new SchemaContainer($app);
+        });
+
+        $this->app->alias('relay', SchemaContainer::class);
+
+        $this->mergeConfigFrom(__DIR__ . '../config/config.php', 'relay');
+    }
+
+    /**
+     * Register schema mutations and queries.
+     *
+     * @return void
+     */
+    protected function registerSchema()
+    {
+        $namespace = config('relay.namespace');
+        $register = config('relay.schema');
+
+        $register($namespace);
     }
 }
