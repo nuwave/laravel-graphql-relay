c509948ffa84f3226be1645c867b493ae6ca45cd
Christopher Moore <chris@nuwavecommerce.com>
2016-01-25 14:11:44 -0700

    updated configuration file

--- config/config.php
+++ config/config.php
@@ -10,17 +10,21 @@
 */
 
 return [
-    // Default namespace of your GraphQL queries and mutations.
-    'namespace' => 'App\\Http\\GraphQL',
-
     // The path where you published your graphql.php file
     // https://github.com/Folkloreatelier/laravel-graphql#installation-1
     'graphql_config' => config_path('graphql.php'),
 
     // Add your mutations and queries here.
-    'schema' => function ($namespace) {
-        Relay::group(['namespace' => $namespace], function () {
-            // ...
+    'schema' => function () {
+        Relay::group(['namespace' => 'Nuwave\\Relay'], function () {
+            Relay::group(['namespace' => 'Node'], function () {
+                Relay::query('node', 'NodeQuery');
+                Relay::type('node', 'NodeType');
+            });
+
+            Relay::type('pageInfo', 'Types\\PageInfoType');
         });
+
+        // Additional Queries, Mutations and Types...
     }
 ];
